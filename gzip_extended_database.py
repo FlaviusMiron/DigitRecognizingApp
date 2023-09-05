14a0d52d1933457dc7d41a0aa2e22121e75a904d
FlaviusMiron <100422650+FlaviusMiron@users.noreply.github.com>
2023-09-05 18:47:30 +0300

    Update gzip_extended_database.py

--- gzip_extended_database.py
+++ gzip_extended_database.py
@@ -1,3 +1,8 @@
+"""
+You can run this script if you want the database to take less space on the computer, at the cost of waitning a bit more time:
+The "extended_minst_loader will have to unzip it every time you train the network."
+"""
+
 import gzip
 
 with open("expanded_training_data.pkl","rb") as input_file, gzip.open("expanded_training_data.pkl.gz","wb") as output_file:
