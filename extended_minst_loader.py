4e05c2151b537039e3f47323306b1bdd09079e58
FlaviusMiron <100422650+FlaviusMiron@users.noreply.github.com>
2023-09-05 19:04:34 +0300

    Update extended_minst_loader.py

--- extended_minst_loader.py
+++ extended_minst_loader.py
@@ -14,13 +14,14 @@ def load_data_wrapper():
     # f = gzip.open('expanded_training_data.pkl.gz', 'rb')
     # training_data = pickle.load(f, encoding="latin1")
     if os.path.isfile("expanded_training_data.pkl"):
-
         file = open("expanded_training_data.pkl","rb")
         training_data = pickle.load(file)
+        
     elif os.path.isfile("expanded_training_data.pkl.gz"):
         print('Un-zipping "expanded_training_data.pkl.gz", might take extra time. You can un-zip it to prevent this from happening.')
         f = gzip.open('expanded_training_data.pkl.gz', 'rb')
         training_data = pickle.load(f, encoding="latin1")
+        
     else:
         raise MissingDatabaseError('Extended database missing. Run the "process_images.py" script to create it.')
 
