12509f62db34e1f2c1766ba4b2166ff1b4dc0698
FlaviusMiron <100422650+FlaviusMiron@users.noreply.github.com>
2023-09-05 18:14:11 +0300

    Update model_training.py

--- model_training.py
+++ model_training.py
@@ -2,7 +2,7 @@
 Script that trains the multi-layer perceptron.
 Will print the accuracy on the unseen, test data.
 Can be trained either with the normal or the extended database (Note: the extended database takes significantly more time to train)
-Comment the corresponding lines to chose the training data. Right now it uses the extended database.
+Comment the corresponding lines to chose the training data. Right now it uses the normal database.
 """
 
 import minst_loader
@@ -10,12 +10,12 @@ import DigitsRecognizingNN
 import gzip
 import pickle
 
-#training_data, validation_data, test_data = minst_loader.load_data_wrapper() # This will train the network with the normal database
+training_data, validation_data, test_data = minst_loader.load_data_wrapper() # This will train the network with the normal database
 
 
-training_data_none, validation_data, test_data = minst_loader.load_data_wrapper() # This will train the network with the extended database
-f = gzip.open('expanded_training_data.pkl.gz', 'rb')
-training_data = pickle.load(f, encoding="latin1")
+# training_data_none, validation_data, test_data = minst_loader.load_data_wrapper() # This will train the network with the extended database
+# f = gzip.open('expanded_training_data.pkl.gz', 'rb')
+# training_data = pickle.load(f, encoding="latin1")
 
 
 model = DigitsRecognizingNN.MLP([784,30,10])
